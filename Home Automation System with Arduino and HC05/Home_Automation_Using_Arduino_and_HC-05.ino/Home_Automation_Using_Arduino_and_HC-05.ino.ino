838eafcec27de3734fc43702014ff5ec6863dc6f
eternaldemon <44200875+eternaldemon@users.noreply.github.com>
2019-07-29 14:00:06 +0530

    Update Home_Automation_Using_Arduino_and_HC-05.ino.ino

--- Home Automation System with Arduino and HC05/Home_Automation_Using_Arduino_and_HC-05.ino/Home_Automation_Using_Arduino_and_HC-05.ino.ino
+++ Home Automation System with Arduino and HC05/Home_Automation_Using_Arduino_and_HC-05.ino/Home_Automation_Using_Arduino_and_HC-05.ino.ino
@@ -1,46 +1,36 @@
-int relay1=2;
-int relay2=3;
+int in1 = 2;
+int in2 = 3;
 char temp;
-
-void setup() 
-{
-pinMode(relay1,OUTPUT);
-pinMode(relay2,OUTPUT);
-digitalWrite(relay1,HIGH);
-digitalWrite(relay2,HIGH);
-Serial.begin(9600);
+void setup() {
+  pinMode(in1, OUTPUT);
+  digitalWrite(in1, HIGH);
+  pinMode(in2, OUTPUT);
+  digitalWrite(in2, HIGH);
+  Serial.begin(9600);
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-  temp = Serial.read();
+void loop() {
+  Serial.flush();
+  if (Serial.available()>0)
+  { temp= Serial.read();
+
+  switch(temp)
+  {case 'a':
+      digitalWrite(in1, LOW);
+      Serial.println("Relay 1 ON");
+      break;
+   case 'b':
+      digitalWrite(in1, HIGH);
+      Serial.println("Relay 1 OFF");
+      break;
+   case 'c':
+      digitalWrite(in2, LOW);
+      Serial.println("Relay 2 ON");
+      break;
+   case 'd':
+      digitalWrite(in2, HIGH);
+      Serial.println("Relay 2 OFF");
+      break;
   }
-
-switch (temp)
-{
-  case '1':
-    digitalWrite(relay1,LOW);
-    Serial.println("Device 1 Turned ON");
-    delay(1500);
-    break;
-  case '0':
-    digitalWrite(relay1,HIGH);
-    Serial.println("Device 1 Turned OFF");
-    delay(1500);
-    break;
-  case '3':
-    digitalWrite(relay2,LOW);
-    Serial.println("Device 2 Turned ON");
-    delay(1500);
-    break;
-  case '2':
-    digitalWrite(relay2,HIGH);
-    Serial.println("Device 2 Turned OFF");
-    delay(1500);
-    break;
-    
-}
-  
+  }
 }
